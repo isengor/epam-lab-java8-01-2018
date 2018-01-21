bc8af720890add51f5ce7808bb5f9389dc1435ae
sysloparich <sysloparich@gmail.com>
2018-01-21 20:55:47 +0300

    Done Excercise3

--- src/test/java/lambda/part1/exercise/Exercise3.java
+++ src/test/java/lambda/part1/exercise/Exercise3.java
@@ -1,5 +1,6 @@
 package lambda.part1.exercise;
 
+import com.google.common.collect.FluentIterable;
 import lambda.data.Person;
 import org.junit.Test;
 
@@ -17,6 +18,7 @@ public class Exercise3 {
         Person[] persons = getPersons();
 
         // TODO использовать Arrays.sort + expression-lambda
+        Arrays.sort(persons, (firstPerson, secondPerson) -> Integer.compare(firstPerson.getAge(), secondPerson.getAge()));
 
         assertArrayEquals(new Person[]{
             new Person("Иван", "Мельников", 20),
@@ -31,6 +33,10 @@ public class Exercise3 {
         Person[] persons = getPersons();
 
         // TODO использовать Arrays.sort + statement-lambda
+        Arrays.sort(persons, (firstPerson, secondPerson) -> {
+            int i = firstPerson.getLastName().compareTo(secondPerson.getLastName());
+            return i != 0 ? i : firstPerson.getFirstName().compareTo(secondPerson.getFirstName());
+        });
 
         assertArrayEquals(new Person[]{
             new Person("Алексей", "Доренко", 40),
@@ -46,6 +52,10 @@ public class Exercise3 {
 
         // TODO использовать FluentIterable
         Person person = null;
+        person = FluentIterable
+                .from(persons)
+                .firstMatch(p -> p.getAge() == 30)
+                .get();
 
         assertEquals(new Person("Николай", "Зимов", 30), person);
     }
