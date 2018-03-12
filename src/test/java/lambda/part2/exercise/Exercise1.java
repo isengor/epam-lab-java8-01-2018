bda42b9dd935a7be86f241a3e88366d0f6ee41fe
isengor <mustbeisen@gmail.com>
2018-03-12 12:49:47 +0300

    lambda.part2.ex1 Done

--- src/test/java/lambda/part2/exercise/Exercise1.java
+++ src/test/java/lambda/part2/exercise/Exercise1.java
@@ -4,9 +4,12 @@ import lambda.data.Person;
 import org.junit.Test;
 
 import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @SuppressWarnings({"unused", "ConstantConditions"})
 public class Exercise1 {
@@ -17,10 +20,13 @@ public class Exercise1 {
 
         // TODO создать переменную ageExtractor: Person -> Integer, используя Function и ссылку на метод
 
-        // assertEquals(33, ageExtractor.apply(person).intValue());
+        Function<Person,Integer> ageExtractor = Person::getAge;
+        assertEquals(33, ageExtractor.apply(person).intValue());
 
-        // FIXME удалить при реализации
-        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    private static boolean isSameAge(Person person1, Person person2) {
+        return person1.getAge() == person2.getAge();
     }
 
     @Test
@@ -30,22 +36,30 @@ public class Exercise1 {
         Person person3 = new Person("Илья", "Жирков", 22);
 
         // TODO создать переменную sameAgesChecker: (Person, Person) -> boolean, используя BiPredicate
+        BiPredicate<Person,Person> sameAgesChecker = Exercise1::isSameAge;
+        assertTrue(sameAgesChecker.test(person1, person2));
+        assertFalse(sameAgesChecker.test(person1, person3));
+        assertFalse(sameAgesChecker.test(person2, person3));
 
-        // assertTrue(sameAgesChecker.test(person1, person2));
-        // assertFalse(sameAgesChecker.test(person1, person3));
-        // assertFalse(sameAgesChecker.test(person2, person3));
-
-        // FIXME удалить при реализации
-        throw new UnsupportedOperationException("Not implemented");
     }
 
     // TODO метод getFullName: Person -> String, извлекающий из объекта Person строку в формате "имя фамилия".
     // private static ... getFullName(...) {
+    private static String getFullName(Person person){
+        return person.getFullName();
+    }
 
     // TODO метод createExtractorAgeOfPersonWithTheLongestFullName: (Person -> String) -> ((Person, Person) -> int),
     // TODO - принимающий способ извлечения полного имени из объекта Person
     // TODO - возвращающий BiFunction, сравнивающий два объекта Person и возвращающий возраст того, чье полное имя длиннее.
     // private static ... createExtractorAgeOfPersonWithTheLongestFullName(...) {
+    private static BiFunction<Person, Person, Integer> createExtractorAgeOfPersonWithTheLongestFullName(Function<Person, String> fullNameExtractor) {
+        return (p1, p2) -> {
+            int p1Name = fullNameExtractor.apply(p1).length();
+            int p2Name = fullNameExtractor.apply(p2).length();
+            return p1Name > p2Name ? p1.getAge() : p2.getAge();
+        };
+    }
 
     @Test
     public void getAgeOfPersonWithTheLongestFullName() {
@@ -53,11 +67,11 @@ public class Exercise1 {
         Person person2 = new Person("Илья", "Жирков", 22);
 
         // TODO воспользоваться ссылкой на метод getFullName
-        Function<Person, String> getFullName = null;
+        Function<Person, String> getFullName = Exercise1::getFullName;
 
         // (Person, Person) -> Integer
         // TODO воспользоваться методом createExtractorAgeOfPersonWithTheLongestFullName
-        BiFunction<Person, Person, Integer> extractorAgeOfPersonWithTheLongestFullName = null;
+        BiFunction<Person, Person, Integer> extractorAgeOfPersonWithTheLongestFullName = createExtractorAgeOfPersonWithTheLongestFullName(getFullName);
 
         assertEquals(33, extractorAgeOfPersonWithTheLongestFullName.apply(person1, person2).intValue());
     }
